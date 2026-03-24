8a46cf9c62c8831b9847d8752ab705645833e12e
Patternscompany <pandureddypatterns@gmail.com>
2026-03-24 12:59:36 +0530

    pp

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -86,7 +86,6 @@ export const AuthProvider = ({ children }) => {
     </AuthContext.Provider>
   );
 };
-
 export const useAuth = () => {
   const ctx = useContext(AuthContext);
   if (!ctx) throw new Error("useAuth must be used within AuthProvider");
